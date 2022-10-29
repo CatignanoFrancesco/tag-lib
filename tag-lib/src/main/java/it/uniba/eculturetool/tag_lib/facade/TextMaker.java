b39f50e57cc5259b12b5e5cfb188e64b2d205941
CatignanoFrancesco <catignanof@gmail.com>
2022-10-29 14:02:26 +0200

    Aggiunta la classe TextMaker

---
+++ tag-lib/src/main/java/it/uniba/eculturetool/tag_lib/facade/TextMaker.java
@@ -0,0 +1,10 @@
+package it.uniba.eculturetool.tag_lib.facade;
+
+import java.util.Map;
+
+/**
+ * Questa classe si occupa di generare dei testi nelle varie lingue partendo da uno in una lingua casuale
+ */
+public class TextMaker {
+    Map<Integer, String> texts;
+}
