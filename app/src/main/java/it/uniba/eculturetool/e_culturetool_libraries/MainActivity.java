40f869d02eaa675974877bbe35df90094c1253a1
CatignanoFrancesco <catignanof@gmail.com>
2022-10-29 13:17:23 +0200

    First commit

---
+++ app/src/main/java/it/uniba/eculturetool/e_culturetool_libraries/MainActivity.java
@@ -0,0 +1,13 @@
+package it.uniba.eculturetool.e_culturetool_libraries;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
