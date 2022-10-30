b6c61c471f62d3fe65a19444c3b923d58128f36f
CatignanoFrancesco <catignanof@gmail.com>
2022-10-30 11:54:09 +0100

    Modificati i costruttori in LanguageTag

--- tag-lib/src/main/java/it/uniba/eculturetool/tag_lib/model/LanguageTag.java
+++ tag-lib/src/main/java/it/uniba/eculturetool/tag_lib/model/LanguageTag.java
@@ -3,16 +3,19 @@ package it.uniba.eculturetool.tag_lib.model;
 public class LanguageTag extends Tag {
     private String language;
 
-    public LanguageTag(int id, String title) {
+    public LanguageTag(int id, String title, String language) {
         super(id, title);
+        this.language = language;
     }
 
-    public LanguageTag(int id, String title, long color) {
+    public LanguageTag(int id, String title, long color, String language) {
         super(id, title, color);
+        this.language = language;
     }
 
-    public LanguageTag(int id, String title, long color, long icon) {
+    public LanguageTag(int id, String title, long color, long icon, String language) {
         super(id, title, color, icon);
+        this.language = language;
     }
 
     public String getLanguage() {
