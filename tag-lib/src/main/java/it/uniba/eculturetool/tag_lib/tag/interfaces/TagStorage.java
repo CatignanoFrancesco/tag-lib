78871a8dc87c823aaa4686d2b8bc3f1b6a76fda7
CatignanoFrancesco <catignanof@gmail.com>
2022-11-02 19:22:38 +0100

    Aggiunti altri metodi in TagStorage

--- tag-lib/src/main/java/it/uniba/eculturetool/tag_lib/tag/interfaces/TagStorage.java
+++ tag-lib/src/main/java/it/uniba/eculturetool/tag_lib/tag/interfaces/TagStorage.java
@@ -15,7 +15,11 @@ public interface TagStorage {
     Set<Tag> getGeneralTags();
     Set<LanguageTag> getLanguageTags();
     Set<LanguageTag> getPlaceLanguages(int placeId);
+    void addPlaceLanguages(int placeId, Set<LanguageTag> languageTags);
+    void deletePlaceLanguages(int placeId, Set<LanguageTag> languageTags);
     Set<Tag> getPathTags(int pathId);
+    void addPathTags(int pathId, Set<Tag> tags);
+    void deletePathTags(int pathId, Set<Tag> tags);
     void updateTag(Tag tag);
     void deleteTagById(int id);
 }
